7c6b8ec0496a353ccc915c8fcc429929dc33518d
Josue_prado <prado69508698@gmial.com>
2024-06-12 12:59:51 -0400

    add bash scripting

---
+++ problems/classwork/scripts/script-1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Running scrip at: " `date`
+
+echo "Enter a directory path: "
+read dir_path
+echo "Conentend of the directory:"
+ls $dir_path 
+find $dir_path -name "*.log"
+
+if ([ -lt -gt > <]): then 
+    # code
+fi 
+
+# while []
+# for []
+# case
+# swithc
+# fun(sdf) { }
+# fun();
+
+echo $1
